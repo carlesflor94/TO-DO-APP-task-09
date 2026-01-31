29b4a20172b12a8e21c6713edde82d0239afa9f9
carlesflor94 <cf.ferrandis@gmail.com>
2026-01-31 15:34:49 +0100

    setup html, css and jsx

--- vite-task-09/src/App.jsx
+++ vite-task-09/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
+      </header>
+
+      <section className="main">
+        <ul className="todo-list">
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>
+                <span className="description">Completed task</span>
+                <span className="created">created 17 seconds ago</span>
+              </label>
+              <button className="icon icon-edit"></button>
+              <button className="icon icon-destroy"></button>
+            </div>
+          </li>
+        </ul>
+
+        <footer className="footer">
+          <span className="todo-count">1 item left</span>
+        </footer>
+      </section>
+    </section>
   )
 }
 
