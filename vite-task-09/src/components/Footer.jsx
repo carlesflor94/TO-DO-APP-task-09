a4b401d510e9e282d64bffb7716679f8616da462
carlesflor94 <cf.ferrandis@gmail.com>
2026-01-31 16:02:24 +0100

    added footer and tasksfilter

--- vite-task-09/src/components/Footer.jsx
+++ vite-task-09/src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import TasksFilter from "./TasksFilter";
+
 const Footer = () => {
     return (
         <footer className="footer">
           <span className="todo-count">1 item left</span>
+          <TasksFilter />
+          <button className="clear-completed">Clear completed</button>
         </footer>
     );
 }
