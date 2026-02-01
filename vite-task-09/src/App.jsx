330d2351dae9af6ac6bea7a411276b0c17fcfbe4
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-01 01:39:33 +0100

    eslint

--- vite-task-09/src/App.jsx
+++ vite-task-09/src/App.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react'
-import Task from './components/Task';
+import { useState } from 'react';
 import TaskList from './components/TaskList';
 import NewTaskForm from './components/NewTaskForm';
 import Footer from './components/Footer';
 
 function App() {
-
   const [tasks, setTasks] = useState([
-    {
-      description: 'New task',
-      time: new Date(),
-    }
-  ])
+    { description: 'New task', time: new Date() },
+  ]);
 
   return (
     <section className="todoapp">
       <NewTaskForm />
 
       <section className="main">
-        <TaskList tasks={tasks}/>
-
+        <TaskList tasks={tasks} />
         <Footer />
       </section>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
